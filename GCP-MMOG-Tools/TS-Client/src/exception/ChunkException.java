d70521acfb270a5928be9e78d7647217ea381f0a
Nicos Kasenides <hfnovemeber@hotmail.com>
2020-02-13 16:24:51 +0200

    Initial Commit

---
+++ GCP-MMOG-Tools/TS-Client/src/exception/ChunkException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class ChunkException extends Exception {
+
+    public ChunkException(String message) {
+        super(message);
+    }
+
+}
---
+++ GCP-MMOG-Tools/TS-Client/src/model/Position2D.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Position2D {
+
+    private long x;
+    private long y;
+
+    public Position2D(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
+
+}
