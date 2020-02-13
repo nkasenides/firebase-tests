9f85635744c84d51e037f94bb5cb320de437c69e
Nicos Kasenides <hfnovemeber@hotmail.com>
2020-02-13 16:48:20 +0200

    triggers

--- GCP-MMOG-Tools/TS-Client/src/Main.java
+++ GCP-MMOG-Tools/TS-Client/src/Main.java
@@ -1,8 +1,9 @@
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.database.annotations.Nullable;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -23,7 +24,36 @@ public class Main {
         initialize();
         System.out.println(" [DONE]");
 
+        DocumentReference docRef = firestore.collection("chunks").document("163e4974-22a4-42df-aedf-6ac6e5faebbe");
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirestoreException e) {
+                if (e != null) {
+                    System.err.println("Listen failed: " + e);
+                    return;
+                }
 
+                if (snapshot != null && snapshot.exists()) {
+                    System.out.println("Current data: " + snapshot.getData());
+                } else {
+                    System.out.print("Current data: null");
+                }
+            }
+        });
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {}
+            }
+        });
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t.start();
 
 
     }
